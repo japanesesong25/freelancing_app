b21f18391de644f4802f318d19b81398862b9824
Sarthak-Singh25 <166988437+Sarthak-Singh25@users.noreply.github.com>
2024-11-09 20:52:04 +0545

    final

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -4,7 +4,7 @@ import Conversation from "../models/conversation.model.js";
 export const createConversation = async (req, res, next) => {
   const newConversation = new Conversation({
     id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
-    sellerId: req.isSeller ? req.userId : req.body.to,
+    sellerId: req.isSeller ?req.userId : req.body.to,
     buyerId: req.isSeller ? req.body.to : req.userId,
     readBySeller: req.isSeller,
     readByBuyer: !req.isSeller,
